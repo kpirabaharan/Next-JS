1fe21fd881e813488beb1d76d13f1e22132d02a0
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-06-09 19:58:48 -0400

    Section 3: Navigating to Dynamic Routes
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- section-3-file-based-routing/pages/clients/index.js
+++ section-3-file-based-routing/pages/clients/index.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import Link from 'next/link';
 
-const ClientPage = () => {
+const ClientsPage = () => {
+  const clients = [
+    { id: 'keesh', name: 'Keeshigan' },
+    { id: 'binu', name: 'Biranugan' },
+  ];
   return (
     <div>
       <h1>Client Page</h1>
+
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link href={`/clients/${client.id}`}>{client.name}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
 
-export default ClientPage;
+export default ClientsPage;

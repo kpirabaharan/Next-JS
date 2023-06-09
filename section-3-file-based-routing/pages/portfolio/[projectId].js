3c016045c52189b3b1778af79c3600316caacdaf
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-06-08 22:52:44 -0400

    Section 3: Extracting Dynamic Path Segment Data
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- section-3-file-based-routing/pages/portfolio/[projectId].js
+++ section-3-file-based-routing/pages/portfolio/[projectId].js
@@ -1,9 +1,14 @@
-import React from 'react';
+import { useRouter } from 'next/router';
 
 const PortfolioProjectPage = () => {
+  const router = useRouter();
+
+  console.log(router.pathname);
+  console.log(router.query);
+
   return (
     <div>
-      <h1>Portfolio Project Page</h1>
+      <h1>Portfolio Project Page {router.query.projectId}</h1>
     </div>
   );
 };

8ad0e787480c3968fb63d7a3a1bac8c7091a7089
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-06-08 20:16:51 -0400

    Section 3: Added Nested Routes
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

---
+++ section-3-file-based-routing/pages/portfolio/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PortfolioPage = () => {
+  return (
+    <div>
+      <h1>The Portfolio Page</h1>
+    </div>
+  );
+};
+
+export default PortfolioPage;
---
+++ section-3-file-based-routing/pages/portfolio/list.js
@@ -0,0 +1,9 @@
+const ListPage = () => {
+  return (
+    <div>
+      <h1>Portfolio List Page</h1>
+    </div>
+  );
+};
+
+export default ListPage;

2ee96199ee1e0a9535f51fa43cb2124aaf4fb1c4
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-06-10 11:28:41 -0400

    Section 5: Running Server-side code using FS
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- section-5-pre-rendering/pages/index.js
+++ section-5-pre-rendering/pages/index.js
@@ -1,19 +1,29 @@
+import path from 'path';
+import fs from 'fs/promises';
+
 function HomePage(props) {
   const { products } = props;
 
   return (
     <ul>
       {products.map((prod) => (
-        <li key={prod.id}>{prod.title}</li>
+        <li key={prod.id}>
+          <h1>{prod.title}</h1>
+          <p>{prod.description}</p>
+        </li>
       ))}
     </ul>
   );
 }
 
 export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
   return {
     props: {
-      products: [{ id: 'p1', title: 'Product 1' }],
+      products: data.products,
     },
   };
 }

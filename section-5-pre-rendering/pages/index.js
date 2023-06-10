952c19356f1450ab9657d5fb1fb5f68113b6cb40
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-06-10 11:46:40 -0400

    Section 5: getStaticProps Config
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- section-5-pre-rendering/pages/index.js
+++ section-5-pre-rendering/pages/index.js
@@ -16,15 +16,32 @@ function HomePage(props) {
   );
 }
 
-export async function getStaticProps() {
+export async function getStaticProps(context) {
+  console.log('Re-generate');
+
   const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
+
+  // If data could not be obtained from backend, we can redirect to another page.
+  if (!data) {
+    return {
+      redirect: {
+        destination: '/no-data',
+      },
+    };
+  }
+
+  // If data arr is empty we can show error page.
+  if (data.products.length === 0) {
+    return { notFound: true };
+  }
 
   return {
     props: {
       products: data.products,
     },
+    revalidate: 10,
   };
 }
 

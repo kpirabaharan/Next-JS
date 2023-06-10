e64a1c122cc1c872ba0c5f1470418c968509d551
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-06-10 13:47:26 -0400

    Section 5: Server Side Props
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

---
+++ section-5-pre-rendering/pages/user-profile.js
@@ -0,0 +1,20 @@
+const UserProfilePage = (props) => {
+  return <h1>{props.username}</h1>;
+};
+
+export async function getServerSideProps(context) {
+  const { params, req, res } = context;
+
+  // console.log(req);
+  // console.log(res);
+
+  
+
+  return {
+    props: {
+      username: 'Keeshigan',
+    },
+  };
+}
+
+export default UserProfilePage;

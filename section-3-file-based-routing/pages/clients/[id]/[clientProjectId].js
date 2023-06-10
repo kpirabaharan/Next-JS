b49b019f3fb7648cf58f13ac46e30d589f0478ed
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-06-09 20:52:02 -0400

    Section 3: Navigate Programmatically
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- section-3-file-based-routing/pages/clients/[id]/[clientProjectId].js
+++ section-3-file-based-routing/pages/clients/[id]/[clientProjectId].js
@@ -1,10 +1,8 @@
-import React from 'react';
 import { useRouter } from 'next/router';
 
 const SelectedClientProjectPage = () => {
   const router = useRouter();
 
-  console.log(router.query);
   return (
     <div>
       <h1>Project page for a specific project for a selected client</h1>
--- section-3-file-based-routing/pages/clients/[id]/index.js
+++ section-3-file-based-routing/pages/clients/[id]/index.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import { useRouter } from 'next/router';
+import PortfolioProjectPage from '../../portfolio/[projectId]';
 
 const ClientsProjectsPage = () => {
+  const router = useRouter();
+
+  const { id } = router.query;
+
+  const loadProjectHandler = () => {
+    // router.push('/clients/keesh/projectA');
+    router.push({
+      pathname: '/clients/[id]/[clientProjectId]',
+      query: { id: id, clientProjectId: 'projectA' },
+    });
+    // Cannot go back when replaced
+    // router.replace('/clients/keesh/projectA');
+  };
+
   return (
     <div>
       <h1>The projects of a given client</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 };

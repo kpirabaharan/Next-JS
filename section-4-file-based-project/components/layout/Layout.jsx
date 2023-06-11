8a3a0b6a01f966996e153d393a68a8825feb768c
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-06-11 16:40:42 -0400

    Section 4: Created Wrapper Component for Generic Layout with Navbar
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

---
+++ section-4-file-based-project/components/layout/Layout.jsx
@@ -0,0 +1,12 @@
+import MainHeader from './MainHeader';
+
+const Layout = (props) => {
+  return (
+    <>
+      <MainHeader />
+      <main>{props.children}</main>
+    </>
+  );
+};
+
+export default Layout;

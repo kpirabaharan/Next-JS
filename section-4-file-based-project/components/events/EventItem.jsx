4a8c730f3a47c9d762d94140c1c56f2152f2260a
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-06-11 16:15:24 -0400

    Section 4: Added Buttons and Icons
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- section-4-file-based-project/components/events/EventItem.jsx
+++ section-4-file-based-project/components/events/EventItem.jsx
@@ -1,6 +1,8 @@
-import Link from 'next/link';
-
 import classes from './EventItem.module.css';
+import Button from '../ui/Button';
+import DateIcon from './icons/DateIcon';
+import AddressIcon from './icons/AddressIcon';
+import ArrowRightIcon from './icons/ArrowRightIcon';
 
 const EventItem = (props) => {
   const { title, image, date, location, id } = props;
@@ -22,14 +24,21 @@ const EventItem = (props) => {
         <div className={classes.summary}>
           <h2>{title}</h2>
           <div className={classes.date}>
+            <DateIcon />
             <time>{formattedDate}</time>
           </div>
           <div className={classes.address}>
+            <AddressIcon />
             <address>{formattedAddress}</address>
           </div>
         </div>
         <div className={classes.actions}>
-          <Link href={formattedLink}>Explore Event</Link>
+          <Button link={formattedLink}>
+            <span>Explore Event</span>
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
         </div>
       </div>
     </li>

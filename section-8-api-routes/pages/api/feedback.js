363c2c1e3de4baf1f0dd7077a52cd9ebf16a057c
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-06-13 21:23:34 -0400

    Section 8: Parsing Incoming Request and Executing Server Side Code
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- section-8-api-routes/pages/api/feedback.js
+++ section-8-api-routes/pages/api/feedback.js
@@ -1,3 +1,24 @@
+import fs from 'fs';
+import path from 'path';
+
 export default (req, res) => {
-  res.status(200).json({ message: 'This works!' });
+  if (req.method === 'POST') {
+    const { email, text } = req.body;
+
+    const newFeedback = {
+      id: new Date().toISOString(),
+      email,
+      text,
+    };
+
+    const filePath = path.join(process.cwd(), 'data', 'feedback.json');
+    const fileData = fs.readFileSync(filePath);
+    const data = JSON.parse(fileData);
+    data.push(newFeedback);
+    fs.writeFileSync(filePath, JSON.stringify(data));
+
+    res.status(201).json({ message: 'Success', feedback: newFeedback });
+  } else {
+    res.status(200).json({ message: 'This works!' });
+  }
 };
--- section-8-api-routes/pages/index.js
+++ section-8-api-routes/pages/index.js
@@ -1,7 +1,32 @@
+import { useRef } from 'react';
+
 function HomePage() {
+  const emailInputRef = useRef();
+  const feedbackInputRef = useRef();
+
+  const submitFormHandler = (event) => {
+    event.preventDefault();
+
+    const email = emailInputRef.current.value;
+    const text = feedbackInputRef.current.value;
+
+    fetch();
+  };
+
   return (
     <div>
       <h1>The Home Page</h1>
+      <form type='email'>
+        <div>
+          <label htmlFor='email'>Your Email Address</label>
+          <input type='email' id='email' ref={emailInputRef} />
+        </div>
+        <div>
+          <label htmlFor='feedback'>Your Feedback </label>
+          <textarea id='feedback' rows='5' ref={feedbackInputRef} />
+        </div>
+        <button>Send Feedback</button>
+      </form>
     </div>
   );
 }

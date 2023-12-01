d9f24990929869e3f32d491ad425ae33c2535299
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-11-30 21:08:33 -0500

    Next 14 Server Actions: Load More Anime
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- server-actions/components/LoadMore.tsx
+++ server-actions/components/LoadMore.tsx
@@ -1,10 +1,36 @@
+'use client';
+
+import { useEffect, useState } from 'react';
 import Image from 'next/image';
+import { useInView } from 'react-intersection-observer';
+import { fetchAnime } from '@/app/action';
+import AnimeCard, { AnimeProp } from './AnimeCard';
 
 function LoadMore() {
+  const { ref, inView } = useInView();
+  const [page, setPage] = useState<number>(1);
+  const [data, setData] = useState<AnimeProp[]>([]);
+
+  useEffect(() => {
+    const fetchMoreAnime = async (page: number) => {
+      if (inView) {
+        const newData = await fetchAnime(page + 1);
+        setData(prev => [...prev, ...newData]);
+        setPage(page + 1);
+      }
+    };
+    fetchMoreAnime(page);
+  }, [inView, data, page]);
+
   return (
     <>
+      <section className='grid grid-cols-1 gap-10 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
+        {data.map((item: AnimeProp, index: number) => (
+          <AnimeCard key={item.id} anime={item} index={index} />
+        ))}
+      </section>
       <section className='flex w-full items-center justify-center'>
-        <div>
+        <div ref={ref}>
           <Image
             src='./spinner.svg'
             alt='spinner'
